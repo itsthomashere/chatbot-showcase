c2c3c66acac8bf939ed54005492b041a5d6deafd
itsthomashere <thomasadam.leigh@gmail.com>
2023-11-30 17:01:08 +0200

    Adding update_table function to donations_page

--- sql_tables.py
+++ sql_tables.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from sqlalchemy import create_engine, text
 
 def get_sql_dataframe(table_name: str) -> None:
     conn = st.connection("digitalocean", type="sql")
@@ -6,12 +7,37 @@ def get_sql_dataframe(table_name: str) -> None:
     messages = conn.query(query)
     st.dataframe(messages, use_container_width=True, hide_index=False)
 
+def update_table(table_name, donation_data):
+    query = f"""
+    INSERT INTO {table_name} (date_received, product_code, product_name, category, price, weight, quantity, total_price, total_weight)
+    VALUES (:date_received, :product_code, :product_name, :category, :price, :weight, :quantity, :total_price, :total_weight);
+    """
+    conn = st.connection("digitalocean", type="sql")
+    with conn.session as s:
+        s.execute(text(query), **donation_data)
+        s.commit()
+
+
+def donations_dataset():
+    product_details = {
+        'date_received': '2023-11-30',
+        'product_code': '20000370',
+        'product_name': 'Pork Bangers 500 g',
+        'category': 'Mixed Groceries',
+        'price': 59.99,
+        'weight': 0.5,
+        'quantity': 1,
+        'total_price': 59.99,
+        'total_weight': 0.5
+    }
+
+    get_sql_dataframe('donation_history')
+    dummy_data = st.button("Send dummy data...")
+    if dummy_data:
+        update_table('donation_history', **product_details)
+
+
 def food_dataset():
     st.write("Hello from Woolworths Dataset")
     get_sql_dataframe('dataset')
-
-def donations_dataset():
-    st.write("Hello from Donations")
-    get_sql_dataframe('donation_history')
-
 
