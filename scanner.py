69308db29a766554f81b761907577ad9401aef9a
itsthomashere <thomasadam.leigh@gmail.com>
2023-11-30 16:21:40 +0200

    Configuring function calling dictionary instead of elif statements

--- scanner.py
+++ scanner.py
@@ -1,3 +1,4 @@
+import streamlit as st
 
 def receive_barcodes():
     st.write("Enter a barcode")
--- sql_tables.py
+++ sql_tables.py
@@ -1,3 +1,4 @@
+import streamlit as st
 
 def food_dataset():
     st.write("Hello from Woolworths Dataset")
