e2c7173c3ef5406d3f236117c9764d1927a30f5c
itsthomashere <thomasadam.leigh@gmail.com>
2023-11-30 15:47:40 +0200

    Removing the fluff

--- app.py
+++ app.py
@@ -21,66 +21,12 @@ def customize_streamlit_ui() -> None:
                 """
     st.markdown(hide_st_style, unsafe_allow_html=True)
 
-
-
 customize_streamlit_ui()
 st.title('Woolworths Project')
 
 options = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
     icons=['house', 'cloud-upload', "list-task", 'gear'], 
     menu_icon="cast", default_index=0, orientation="horizontal")
-
-
-def determine_image(role: str, content: str) -> str:
-    """Determine the image file path based on role and content."""
-    
-    # List of greetings to check against
-    greetings: list[str] = ['Hello', 'Hi', 'Hey', 'Greetings', 'Howdy', 'What\'s up', 'Wassup']
-    
-    # Role is 'assistant'
-    if role == 'assistant':
-        return "./icons/assistant.png"
-    
-    # Role is 'user' and content starts with a greeting
-    elif role == 'user' and any(content.startswith(greeting) for greeting in greetings):
-        return "./icons/hello.png"
-    
-    # Role is 'user' but content doesn't start with a greeting
-    elif role == 'user':
-        return "./icons/user.png"
-    
-    # Other cases
-    else:
-        return "./icons/default.png"  # You can define any default case here
-
-def check_keywords(response: str) -> None:
-    keywords = ["Title: ", "Topics: ", "Elaboration: "]
-    if all(keyword in response for keyword in keywords):
-        st.markdown("You can view the Obsidian graph [here](https://publish.obsidian.md/ideavault).")
-        update_session_state(role="assistant", content="You can view the Obsidian graph [here](https://publish.obsidian.md/ideavault).")
-
-#@st.cache_data
-def init_user_id() -> str:
-    """Initialize or retrieve the user ID stored in session_state."""
-    return str(uuid.uuid4())
-
-
-def create_chat_completion(model: str, messages: list[dict[str, str]]) -> None:
-    """Generate and display chat completion using OpenAI and Streamlit."""
-    with st.chat_message(name="assistant", avatar="./icons/assistant.png"):
-        message_placeholder = st.empty()
-        full_response = ""
-        for response in openai.ChatCompletion.create(
-            model=model,
-            messages=messages,
-            stream=True,
-        ):
-            full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-
-        #check_keywords(full_response)
-    return full_response
 
 def create_tables() -> None:
     conn = st.experimental_connection("digitalocean", type="sql")
@@ -127,102 +73,16 @@ def get_sql_dataframe(table_name: str, uuid: str) -> None:
     messages = conn.query(query, ttl=timedelta(minutes=1), params={"uuid": uuid})
     st.dataframe(messages)
 
-
-def init_chat_history() -> None:
-    if "messages" not in st.session_state:
-        st.session_state["messages"] = [{"role": "system", "content": c.SYSTEM_PROMPT}]
-
-
-def update_session_state(role: str, content: str) -> None:
-    """Append a message with role and content to st.session_state["messages"]."""
-    st.session_state["messages"].append({
-        "role": role, 
-        "content": content})
-
-
-def display_chat_history() -> None:
-    """Display conversation history every time there's a new interaction from the user."""
-    for message in st.session_state["messages"]:
-        if message["role"] != "system":
-            with st.chat_message(
-                name=message["role"], 
-                avatar=determine_image(role=message["role"], content=message["content"])
-            ):
-                st.write(message["content"])
-
-
-def display_message(role: str, content: str) -> None:
-    with st.chat_message(
-        name=role, 
-        avatar=determine_image(role=role, content=content)):
-        st.write(content)
-
-def display_welcome_info() -> None:
-    st.title("How to leverage AI for social good.")
-    st.markdown(c.ABOUT_SEGMENT)
-    st.header("💡 → 🤖 → 🕸️ ")
-
-# --- CONFIGURE API KEY ---
-openai.api_key = st.secrets["OPENAI_API_KEY"]
-
-
 try:
     create_tables()
 except Exception:
     pass
 
-if "uuid" not in st.session_state:
-    st.session_state["uuid"] = init_user_id()
-
 # --- INIT SESSION_STATE MESSAGES---
 init_chat_history()
 
-
-# --- SETUP TEMPORARY TITLE & DESCRIPTION ---
-if len(st.session_state["messages"]) == 1:
-    display_welcome_info()
-
-display_chat_history()
-
 # --- USER INTERACTION ---
-user_message = st.chat_input("Present an idea")
-if user_message:
+user_input = st.text_input("Enter a barcode")
+if user_input:
     # --- DISPLAY MESSAGE TO STREAMLIT UI, UPDATE SQL, UPDATE SESSION STATE ---
-    display_message(role="user", content=user_message)
-    try:
-        save_to_sql(user_id=st.session_state["uuid"], role="user", content=user_message)
-    except Exception:
-        pass
-    update_session_state(role="user", content=user_message)
-    
-
-
-    # --- PASS THE ENTIRETY OF SESSION STATE MESSAGES TO OPENAI ---
-    try:
-        response = create_chat_completion(
-            model="gpt-4", 
-            messages=st.session_state["messages"]
-        ) # create_chat_completion already displays message to streamlit UI
-
-    except Exception as e:
-        error_message = f"Error: {str(e)}"
-        st.write(error_message)
-        #self.status_update(False, error_message)
-        # CustomApplication.processEvents()
-        pass
-
-
-    # --- DISPLAY MESSAGE TO STREAMLIT UI, UPDATE SQL, UPDATE SESSION STATE ---
-    try:
-        save_to_sql(user_id=st.session_state["uuid"], role="assistant", content=response)
-    except Exception:
-        pass
-    update_session_state(role="assistant", content=response)
-
-    keywords = ["Title: ", "Topics: ", "Elaboration: "]
-    if all(keyword in response for keyword in keywords):
-        obsidian_link = "You can view the Obsidian graph [here](https://publish.obsidian.md/ideavault)."
-        display_message(role="assistant", content=obsidian_link)
-        update_session_state(role="assistant", content="You can view the Obsidian graph [here](https://publish.obsidian.md/ideavault).")
-
 
