4d55c6a1d14c667d2528b9b56ff99c0617b7827b
itsthomashere <thomasadam.leigh@gmail.com>
2023-11-30 15:31:48 +0200

    Removing exception handling from openai

--- app.py
+++ app.py
@@ -200,34 +200,6 @@ if user_message:
             messages=st.session_state["messages"]
         ) # create_chat_completion already displays message to streamlit UI
 
-    except openai.error.Timeout as e:
-        # Handle timeout error, e.g. retry or log
-        print(f"OpenAI API request timed out: {e}")
-        pass
-    except openai.error.APIError as e:
-        # Handle API error, e.g. retry or log
-        print(f"OpenAI API returned an API Error: {e}")
-        pass
-    except openai.error.APIConnectionError as e:
-        # Handle connection error, e.g. check network or log
-        print(f"OpenAI API request failed to connect: {e}")
-        pass
-    except openai.error.InvalidRequestError as e:
-        # Handle invalid request error, e.g. validate parameters or log
-        print(f"OpenAI API request was invalid: {e}")
-        pass
-    except openai.error.AuthenticationError as e:
-        # Handle authentication error, e.g. check credentials or log
-        print(f"OpenAI API request was not authorized: {e}")
-        pass
-    except openai.error.PermissionError as e:
-        # Handle permission error, e.g. check scope or log
-        print(f"OpenAI API request was not permitted: {e}")
-        pass
-    except openai.error.RateLimitError as e:
-        # Handle rate limit error, e.g. wait or log
-        print(f"OpenAI API request exceeded rate limit: {e}")
-        pass
     except Exception as e:
         error_message = f"Error: {str(e)}"
         print(error_message)
