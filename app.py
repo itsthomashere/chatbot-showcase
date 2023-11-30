317cf3eec7ae9fc12d77e9f4702cc24b4e4e333a
itsthomashere <thomasadam.leigh@gmail.com>
2023-11-30 15:58:34 +0200

    Modifying option menu

--- app.py
+++ app.py
@@ -28,63 +28,52 @@ title = st.markdown(
     f"<h1 style='text-align: center;'>{title}</h1>", unsafe_allow_html=True
 )
 
-options = option_menu(None, ["Donation History", "Barcode Scanner", "Dataset"], 
+options = option_menu(None, ["Donations", "Barcode Scanner", "Dataset"], 
     icons=['house', 'cloud-upload', "list-task"], 
     menu_icon="cast", default_index=1, orientation="horizontal")
+
 
 def create_tables() -> None:
     conn = st.experimental_connection("digitalocean", type="sql")
     with conn.session as s:
-        # Create the 'users' table with a timestamp column
+        # Create the 'donation_history' table with specified columns
         s.execute(text("""
-                    CREATE TABLE IF NOT EXISTS users (
-                    ID SERIAL PRIMARY KEY,
-                    uuid VARCHAR(36) UNIQUE,
-                    timestamp TIMESTAMPTZ);"""))
-        
-        # Create the 'submissions' table with a foreign key relation to 'users'
-        s.execute(text("""
-                    CREATE TABLE IF NOT EXISTS submissions (
-                    ID SERIAL PRIMARY KEY,
-                    uuid VARCHAR(36),
-                    timestamp TIMESTAMPTZ,
-                    role VARCHAR(9) CHECK (LENGTH(role) >= 4),
-                    content TEXT,
-                    FOREIGN KEY (uuid) REFERENCES users(uuid));"""))
+                    CREATE TABLE IF NOT EXISTS donation_history (
+                    product_code VARCHAR(13),
+                    product_name VARCHAR(255),
+                    category VARCHAR(255),
+                    price NUMERIC(10, 2),
+                    weight NUMERIC(10, 2),
+                    quantity INT,
+                    total_price NUMERIC(10, 2),
+                    total_weight NUMERIC(10, 2));"""))
         s.commit()
 
-def save_to_sql(user_id: str, role: str, content: str) -> None:
+
+def check_existing_entry(table_name: str, product_code: str) -> tuple | None:
     conn = st.experimental_connection("digitalocean", type="sql")
-    
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with conn.session as s:
-        # Insert user_id and timestamp into 'users' table if it doesn't already exist
-        s.execute(
-            text('INSERT INTO users (uuid, timestamp) VALUES (:uuid, :timestamp) ON CONFLICT (uuid) DO NOTHING;'),
-            params=dict(uuid=user_id, timestamp=timestamp)
-        )
-        
-        # Insert into 'submissions' table
-        s.execute(
-            text('INSERT INTO submissions (uuid, timestamp, role, content) VALUES (:uuid, :timestamp, :role, :content);'),
-            params=dict(uuid=user_id, timestamp=timestamp, role=role, content=content)
-        )
-        s.commit()
+        query = text(f"""
+            SELECT product_code, product_name, category, price, weight FROM {table_name} WHERE product_code = :product_code
+        """)
+        result = s.execute(query, {"product_code": product_code}).fetchone()
+    return result
 
-def get_sql_dataframe(table_name: str, uuid: str) -> None:
-    conn = st.experimental_connection("digitalocean", type="sql")
-    query = f'select * from {table_name} where uuid = :uuid order by timestamp'
-    messages = conn.query(query, ttl=timedelta(minutes=1), params={"uuid": uuid})
+def get_sql_dataframe(table_name: str) -> None:
+    conn = st.connection("digitalocean", type="sql")
+    query = f'select * from {table_name} order by category'
+    messages = conn.query(query, ttl=timedelta(minutes=1))
     st.dataframe(messages)
 
 try:
     create_tables()
-except Exception:
-    pass
+except Exception as e:
+    st.error(e)
 
 # --- USER INTERACTION ---
 user_input = st.text_input("Enter a barcode")
 if user_input:
     # --- DISPLAY MESSAGE TO STREAMLIT UI, UPDATE SQL, UPDATE SESSION STATE ---
     st.write(user_input)
+
 
