e68e2f456284be78113b3d5818bde50a1f202c9f
itsthomashere <thomasadam.leigh@gmail.com>
2023-11-30 17:12:39 +0200

    Desperately creating an entirely new table donation-history over donation_history

--- sql_tables.py
+++ sql_tables.py
@@ -42,10 +42,10 @@ def donations_dataset():
         'total_weight': 0.5
     }
 
-    get_sql_dataframe('donation_history')
+    get_sql_dataframe('donation-history')
     dummy_data = st.button("Send dummy data...")
     if dummy_data:
-        update_table('donation_history', product_details)
+        update_table('donation-history', product_details)
 
 
 def food_dataset():
