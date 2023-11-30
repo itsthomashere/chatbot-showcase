0ac9238d72ec903b6d6791d2176694f34c77b970
itsthomashere <thomasadam.leigh@gmail.com>
2023-11-30 17:06:17 +0200

    Adding update_table function to donations_page

--- sql_tables.py
+++ sql_tables.py
@@ -7,12 +7,23 @@ def get_sql_dataframe(table_name: str) -> None:
     messages = conn.query(query)
     st.dataframe(messages, use_container_width=True, hide_index=False)
 
+
 def update_table(table_name, donation_data):
+    """Update a SQL table with donation data."""
+    # Define the SQL query with placeholders
     query = f"""
     INSERT INTO {table_name} (date_received, product_code, product_name, category, price, weight, quantity, total_price, total_weight)
     VALUES (:date_received, :product_code, :product_name, :category, :price, :weight, :quantity, :total_price, :total_weight);
     """
+    # Connect to the database
     conn = st.connection("digitalocean", type="sql")
+
+    # Check if donation_data keys match the query placeholders
+    expected_keys = {'date_received', 'product_code', 'product_name', 'category', 'price', 'weight', 'quantity', 'total_price', 'total_weight'}
+    if not expected_keys.issubset(donation_data.keys()):
+        raise ValueError(f"donation_data keys do not match expected keys: {expected_keys}")
+
+    # Execute the query using the donation data
     with conn.session as s:
         s.execute(text(query), **donation_data)
         s.commit()
